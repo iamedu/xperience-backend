6bac56efb5b92738a202caa355830c54c55ed3ca
Eduardo Díaz <iamedu@gmail.com>
2014-01-19 17:06:35 -0600

    Vertx server module

---
+++ server.clj
@@ -0,0 +1,7 @@
+(ns experience.server
+  (:require [vertx.core :as core]))
+
+(core/deploy-module "io.vertx~mod-web-server~2.0.0-final"
+                    :config {:port 8080
+                             :host "localhost"})
+
