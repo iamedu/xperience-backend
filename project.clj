0f95f3c2bc0395fcbd9beb2acdd8c804c20c28ed
Eduardo Díaz <iamedu@gmail.com>
2014-01-19 20:30:35 -0600

    Adding jcodec dependency

--- project.clj
+++ project.clj
@@ -3,9 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [twitter-api "0.7.5"]
+                 [instagram-api "0.1.7"]
+                 [org.jcodec/jcodec "0.1.6-3"]]
   :plugins [[lein-vertx "0.2.0-SNAPSHOT"]]
   :vertx {:main xperience-backend.core/init
-          :author "FIXME: Your name"
-          :keywords ["FIXME: keywords"]
-          :developers ["FIXME: other developers"]})
+          :author "Eduardo Diaz"
+          :keywords ["Video wall"]
+          :developers ["Eduardo Diaz"]})
